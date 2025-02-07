e0b9dc569b1476ae46ce8d2d3a561d2ff82e9802
anitha1204 <selvam12042003@gmail.com>
2025-02-07 14:52:28 +0530

    main

--- controllers/mappingController.js
+++ controllers/mappingController.js
@@ -1,55 +1,12 @@
-const Mapping = require("../models/Mapping");
-
-// Save a new mapping
-exports.saveMapping = async (req, res) => {
-  try {
-    const { almId, almName, qtestId, qtestName, color } = req.body;
-    const newMapping = new Mapping({ almId, almName, qtestId, qtestName, color });
-    await newMapping.save();
-    res.status(201).json({ message: "Mapping saved successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error saving mapping", error });
-  }
-};
-
-// Get all mappings
-exports.getMappings = async (req, res) => {
-  try {
-    const mappings = await Mapping.find();
-    res.status(200).json(mappings);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching mappings", error });
-  }
-};
-
-
 // const Mapping = require("../models/Mapping");
-// const Valuefile = require("../models/almModel");
 
-// // Save a new mapping with almName and its corresponding value from Valuefile
+// // Save a new mapping
 // exports.saveMapping = async (req, res) => {
 //   try {
 //     const { almId, almName, qtestId, qtestName, color } = req.body;
-
-//     // Find the corresponding value for almName from Valuefile
-//     const valuefileEntry = await Valuefile.findOne({ name: almName });
-
-//     if (!valuefileEntry) {
-//       return res.status(404).json({ message: "No matching value found for almName" });
-//     }
-
-//     // Create new mapping with almName and its corresponding value
-//     const newMapping = new Mapping({
-//       almId,
-//       almName,
-//       qtestId,
-//       qtestName,
-//       color,
-//       value: valuefileEntry.value, // Store the corresponding value
-//     });
-
+//     const newMapping = new Mapping({ almId, almName, qtestId, qtestName, color });
 //     await newMapping.save();
-//     res.status(201).json({ message: "Mapping saved successfully", newMapping });
+//     res.status(201).json({ message: "Mapping saved successfully" });
 //   } catch (error) {
 //     res.status(500).json({ message: "Error saving mapping", error });
 //   }
@@ -64,3 +21,66 @@ exports.getMappings = async (req, res) => {
 //     res.status(500).json({ message: "Error fetching mappings", error });
 //   }
 // };
+
+
+
+const Mapping = require("../models/Mapping");
+const ValueFile = require("../models/almModel");
+
+// Save new mapping
+const saveMapping = async (req, res) => {
+  try {
+    const newMapping = new Mapping(req.body);
+    await newMapping.save();
+    res.status(201).json({ message: "Mapping saved successfully", data: newMapping });
+  } catch (error) {
+    res.status(500).json({ message: "Error saving mapping", error: error.message });
+  }
+};
+
+// Get all mappings
+const getMappings = async (req, res) => {
+  try {
+    const mappings = await Mapping.find();
+    res.status(200).json(mappings);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching mappings", error: error.message });
+  }
+};
+
+// Map ALM values and store in MongoDB
+const mapAndStoreValues = async (req, res) => {
+  try {
+    const mappings = await Mapping.find();
+    const valuefiles = await ValueFile.find();
+
+    let mappedResults = [];
+
+    mappings.forEach(mapping => {
+      valuefiles.forEach(valuefile => {
+        if (valuefile.entities) {
+          valuefile.entities.forEach(entity => {
+            if (entity.Fields) {
+              entity.Fields.forEach(field => {
+                if (mapping.almName === field.Name) {
+                  field.values.forEach(val => {
+                    mappedResults.push({
+                      almName: mapping.almName,
+                      value: val.value
+                    });
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    });
+
+    res.status(200).json({ message: "Mapping and values retrieved", data: mappedResults });
+  } catch (error) {
+    res.status(500).json({ message: "Error mapping values", error: error.message });
+  }
+};
+
+module.exports = { saveMapping, getMappings, mapAndStoreValues };
--- models/MappedResult.js
+++ models/MappedResult.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const MappedResultSchema = new Schema({
-  almName: { type: String},
-  value: { type: String }
+  almName: { type: String, required: true },
+  value: { type: String, required: true }
 });
 
 module.exports = mongoose.model("MappedResult", MappedResultSchema);
