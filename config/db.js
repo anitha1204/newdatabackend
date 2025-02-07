31e528e678e3520c2194f63c27e491a3f7eae2f7
anitha1204 <selvam12042003@gmail.com>
2025-02-07 14:07:22 +0530

    main

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log('MongoD connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
