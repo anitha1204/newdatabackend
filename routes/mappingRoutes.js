710909e1c22a6bca486d7231ca2bbe7709b26699
anitha1204 <selvam12042003@gmail.com>
2025-02-07 17:11:16 +0530

    main

--- routes/mappingRoutes.js
+++ routes/mappingRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { getMappings, saveMapping } = require("../controllers/mappingController");
+const { getStoredMappings, saveMapping } = require("../controllers/mappingController");
 
 const router = express.Router();
 
-router.get("/get-stored-mappings", getMappings);
+router.get("/get-stored-mappings", getStoredMappings);
 router.post("/save-mapping", saveMapping);
 
 module.exports = router;
